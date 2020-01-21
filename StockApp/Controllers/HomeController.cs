ba0290ec2cacbba4884829f973ff5713b2c38135
UFOturtle <46785628+UFOturtle@users.noreply.github.com>
2020-01-21 16:50:22 -0500

    Updated Stock App

--- StockApp/Controllers/HomeController.cs
+++ StockApp/Controllers/HomeController.cs
@@ -15,6 +15,12 @@ namespace StockApp.Controllers
             return View();
         }
 
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
