bc72fe241a2be335fcab1c71822af716367ebe15
Warner <lwarner@labs.neit.edu>
2020-02-04 21:11:47 -0500

    finished lab 4 part 1, added ViewModel

---
+++ lab4/Lab2Phase1/ViewModels/Home/ShowLanguagesViewModel.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab2Phase1.Models;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+namespace Lab2Phase1.ViewModels.Home
+{
+    public class ShowLanguagesViewModel
+    {
+        public ShowLanguagesViewModel(CultureInfo[] cultures)
+        {
+            CulturesList = cultures.Select(c => new SelectListItem() {Text = c.EnglishName});
+        }
+        public IEnumerable<SelectListItem> CulturesList{ get; private set;}
+    }
+}
